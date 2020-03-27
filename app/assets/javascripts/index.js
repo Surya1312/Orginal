d0b855234be0221de11b5227b907421b38c1d20f
Surya <suryasubramaniank@gmail.com>
2020-03-27 13:09:21 +0530

    Orginal change v1

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,5 +1,6 @@
 if (self === top) {
   // Not in Iframe
+  window.location.assign("https://ssrs-dev.usgboral.com/ReportServer/Pages/ReportViewer.aspx?/OpportunitySQLDW&BU=IN&OpportunityNumber=OPP-285261&rs:Command=Render");
   alert("This canvas app must be included within an iframe");
 }
 
@@ -7,4 +8,5 @@ Sfdc.canvas(function() {
   // Save the token
   Sfdc.canvas.oauth.token(window.signedRequestJson.oauthToken);
   Sfdc.canvas.byId('username').innerHTML = window.signedRequestJson.context.user.fullName;
+  Sfdc.canvas.byId('bu').innerHTML = window.signedRequestJson.context.environment.parameters.bucode;
 });

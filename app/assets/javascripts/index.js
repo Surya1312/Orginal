2251ef77f7f86ac0976e62b927e6d01167737e5a
Surya <suryasubramaniank@gmail.com>
2020-03-28 11:51:01 +0530

    Testing all type of input from salesforce

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -10,4 +10,18 @@ Sfdc.canvas(function() {
   Sfdc.canvas.byId('username').innerHTML = window.signedRequestJson.context.user.fullName;
   Sfdc.canvas.byId('bu').innerHTML = window.signedRequestJson.context.environment.parameters.bucode;
   Sfdc.canvas.byId('oppNo').innerHTML = window.signedRequestJson.context.environment.parameters.oppNumber;
+  Sfdc.canvas.byId('est').innerHTML = window.signedRequestJson.context.environment.parameters.estDate;
+  Sfdc.canvas.byId('stageType').innerHTML = window.signedRequestJson.context.environment.parameters.stage;
+  Sfdc.canvas.byId('rec').innerHTML = window.signedRequestJson.context.environment.parameters.rec;
+  Sfdc.canvas.byId('ph4').innerHTML = window.signedRequestJson.context.environment.parameters.ph4flag;
+  var buCode = window.signedRequestJson.context.environment.parameters.bucode;
+  var OpportunityNumber = window.signedRequestJson.context.environment.parameters.oppNumber;
+  //Forming the variable to return    
+  var URL;
+  URL="https://ssrs-dev.usgboral.com/ReportServer/Pages/ReportViewer.aspx?/OpportunitySQLDW&BU=";
+  URL += buCode;
+  URL += "&OpportunityNumber=";
+  URL += OpportunityNumber;
+  URL += "&rs:Command=Render";
+  Sfdc.canvas.byId('urltocall').innerHTML = URL;
 });
